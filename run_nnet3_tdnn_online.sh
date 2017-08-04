a8f1b13b32628b75835d506b822e832d7c97771d
funcwj <funcwj@foxmail.com>
2017-08-04 15:05:25 +0800

    fix scoring bugs in train_ambase.sh and update run_nnet3_tdnn_online.sh dropping duplicated scripts

--- run_nnet3_tdnn_online.sh
+++ run_nnet3_tdnn_online.sh
@@ -3,28 +3,34 @@
 # train ivector, extract feature and tdnn training, testing
 # wujian@2017.7.5
 
-
-. ./path.sh
-
-
 stage=0
 
-
 gmm_dir=exp/sat1
-ali_dir=exp/sat1_sp_ali
-exp_dir=/home2/wujian/tdnn_sp
+ali_dir=exp/nnet3/sat1_sp_ali
+exp_dir=exp/nnet3/tdnn_sp
 gph_dir=exp/sat1/graph
 train_cmd=run.pl
 ivector_extractor=exp/nnet3/extractor
 
+train_stage=-10
+common_egs_dir=
+
+initial_effective_lrate=0.0015
+final_effective_lrate=0.00015
+num_epochs=4
+num_jobs_initial=2
+num_jobs_final=8
+remove_egs=true
+
 set -e
+. ./path.sh || exit 1
 . parse_options.sh || exit 1
 
 # 准备dev_hires, train_hires 调整音量
 # 特征是mfcc+online+pitch 
 # 还准备了去除pitch信息的特征
 if [ $stage -le 1 ]; then
-
+    echo "$0: 1. PREPARE ONLINE-MFCC-HIRES..."
     for dir in train dev; do utils/copy_data_dir.sh data/train data/${dir}_hires; done
     tr_hires="data/train_hires"
 
@@ -45,7 +51,7 @@ for line in sys.stdin.readlines():
 
         steps/make_mfcc_pitch_online.sh --nj 10 --mfcc-config conf/mfcc_hires.conf --cmd "$train_cmd" \
             data/$dir exp/make_hires/$dir mfcc_hires || exit 1;
-        steps/compute_cmvn_stats.sh data/dir exp/make_hires/$dir mfcc_hires || exit 1;
+        steps/compute_cmvn_stats.sh data/$dir exp/make_hires/$dir mfcc_hires || exit 1;
 
         # make MFCC data dir without pitch to extract iVector
         utils/data/limit_feature_dim.sh 0:39 data/$dir data/${dir}_nopitch || exit 1;
@@ -54,33 +60,35 @@ for line in sys.stdin.readlines():
 fi
 
 # 使用nopitch信息训练一个LDA+MLLT模型
+# nopitch信息和LDA+MLLT模型训练一个ubm
+# nopitch和ubm训练ivector
+
 if [ $stage -le 2 ]; then
+    echo "$0: 2.1 LDA-MLLT TRAINING FOR MFCC-HIRES..."
     steps/train_lda_mllt.sh --cmd "$train_cmd" --num-iters 13 --realign-iters "" \
         --splice-opts "--left-context=3 --right-context=3" \
-        5000 10000 data/train_hires_nopitch data/lang ${gmm_dir}_align exp/nnet3/lda_mllt || exit 1
-fi
+        5000 10000 data/train_hires_nopitch data/lang ${gmm_dir}_ali exp/nnet3/lda_mllt || exit 1
 
-# nopitch信息和LDA+MLLT模型训练一个ubm
-if [ $stage -le 3 ]; then
+    echo "$0: 2.2 TRAINING UBM..."
     steps/online/nnet2/train_diag_ubm.sh --cmd "$train_cmd" --nj 10 \
-        --num-frames 700000 data/train_hires_nopitch 512 exp/nnet3/tri5 exp/nnet3/diag_ubm || exit 1
-fi
+        --num-frames 700000 data/train_hires_nopitch 512 exp/nnet3/lda_mllt exp/nnet3/diag_ubm || exit 1
 
-# nopitch和ubm训练ivector
-if [ $stage -le 4 ]; then
+    echo "$0: 2.3 TRAINING IVECTOR..."
     steps/online/nnet2/train_ivector_extractor.sh --cmd "$train_cmd" --nj 10 \
         data/train_hires_nopitch exp/nnet3/diag_ubm $ivector_extractor || exit 1;
 fi
 
+
 # 准备变速训练集合 train_sp
 # 准备online的pitch和nopitch信息
-if [ $stage -le 5 ]; then
+if [ $stage -le 3 ]; then
+    echo "$0: 3. PREPARE TRAIN_SP..."
     # train + speed pertrub => train_sp
     speed=(0.9 1.0 1.1)
     for idx in $(seq 0 2); do utils/perturb_data_dir_speed.sh ${speed[$idx]} data/train data/temp${idx}; done
 
     utils/combine_data.sh --extra-files utt2uniq data/train_sp data/temp0 data/temp1 data/temp2
-    rm -r data/temp1 data/temp2 data/temp3
+    rm -r data/temp0 data/temp1 data/temp2
 
     for dir in train_sp; do
         steps/make_mfcc_pitch_online.sh --cmd "$train_cmd" --nj 10 \
@@ -108,37 +116,92 @@ fi
 
 # 由train_sp_hires_nopitch生成一个max2数据
 # 提取train/dev ivector
-if [ $stage -le 6 ]; then
+if [ $stage -le 4 ]; then
+    echo "$0: 4.1 EXTRACT IVECTOR FOR TRAIN_SP/DEV..."
+
     steps/online/nnet2/copy_data_dir.sh --utts-per-spk-max 2 \
         data/train_sp_hires_nopitch data/train_sp_hires_nopitch_max2
 
     steps/online/nnet2/extract_ivectors_online.sh --cmd "$train_cmd" --nj 10 \
         data/train_sp_hires_nopitch_max2 $ivector_extractor exp/nnet3/ivectors_train_sp \
-        || echo "$0: error extracting ivectors on training set" && exit 1;
+        || (echo "$0: error extracting ivectors on training set" && exit 1);
 
-    steps/online/nnet2/extract_ivectors_online.sh --cmd "$train_cmd" --nj 8 \
+    steps/online/nnet2/extract_ivectors_online.sh --cmd "$train_cmd" --nj 10 \
         data/dev_hires_nopitch $ivector_extractor exp/nnet3/ivectors_dev \
-        || echo "$0: error extracting ivectors on dev set" && exit 1;
+        || (echo "$0: error extracting ivectors on dev set" && exit 1);
+
+    # lowres + sp的对齐结果，因为gmm_dir是低分辨率的
+    echo "$0: 4.2 PREPARE ALIGNMENTS FOR TRAIN_SP..."
+    steps/align_fmllr.sh --nj 10 --cmd "$train_cmd" data/train_sp data/lang $gmm_dir $ali_dir || exit 1
 fi
 
 
 # 对齐，训练
-if [ $stage -le 7 ]; then 
-    steps/train_fmllr.sh --nj 10 --cmd "$train_cmd" data/train_sp data/lang $gmm_dir $ali_dir || exit 1
+if [ $stage -le 5 ]; then 
+    echo "$0: 5. TRAINING NNET3..."
 
-    steps/nnet3/train_tdnn.sh --num-epochs 4 --num-jobs-initial 2 --num-jobs-final 8 \
-        --splice-indexes "-2,-1,0,1,2 -1,2 -3,3 -7,2 -3,3 0 0" \
-        --feat-type raw \
-        --online-ivector-dir exp/nnet3/ivectors_train_sp \
-        --cmvn-opts "--norm-means=false --norm-vars=false" \
-        --initial-effective-lrate 0.0015 --final-effective-lrate 0.00015 \
-        --cmd "run.pl" \
-        --relu-dim 850 \
-        --stage -3 \
-        data/train_sp_hires data/lang $ali_dir $exp_dir || exit 1;
+    num_targets=$(tree-info $ali_dir/tree | grep num-pdfs | awk '{print $2}')
+    mkdir -p $exp_dir/configs
+
+    cat <<EOF > $exp_dir/configs/network.xconfig
+    input dim=100 name=ivector
+    input dim=43 name=input
+    fixed-affine-layer name=lda input=Append(-2,-1,0,1,2,ReplaceIndex(ivector, t, 0)) affine-transform-file=$exp_dir/configs/lda.mat
+    relu-batchnorm-layer name=tdnn1 dim=850
+    relu-batchnorm-layer name=tdnn2 dim=850 input=Append(-1,0,2)
+    relu-batchnorm-layer name=tdnn3 dim=850 input=Append(-3,0,3)
+    relu-batchnorm-layer name=tdnn4 dim=850 input=Append(-7,0,2)
+    relu-batchnorm-layer name=tdnn5 dim=850 input=Append(-3,0,3)
+    relu-batchnorm-layer name=tdnn6 dim=850
+    output-layer name=output input=tdnn6 dim=$num_targets max-change=1.5
+EOF
+
+  steps/nnet3/xconfig_to_configs.py --xconfig-file $exp_dir/configs/network.xconfig --config-dir $exp_dir/configs/
+
+#    has been deplicated
+#    python steps/nnet3/tdnn/make_configs.py  \
+#        --feat-dir data/train_sp_hires \
+#        --ivector-dir exp/nnet3/ivectors_train_sp \
+#        --ali-dir $ali_dir \
+#        --relu-dim 850 \
+#        --splice-indexes "-2,-1,0,1,2 -1,2 -3,3 -7,2 -3,3 0 0"  \
+#        --use-presoftmax-prior-scale true \
+#        $exp_dir/configs || exit 1;
+
+
+    steps/nnet3/train_dnn.py --stage=$train_stage \
+        --cmd="run.pl" \
+        --feat.online-ivector-dir exp/nnet3/ivectors_train_sp \
+        --feat.cmvn-opts="--norm-means=false --norm-vars=false" \
+        --trainer.num-epochs $num_epochs \
+        --trainer.optimization.num-jobs-initial $num_jobs_initial \
+        --trainer.optimization.num-jobs-final $num_jobs_final \
+        --trainer.optimization.initial-effective-lrate $initial_effective_lrate \
+        --trainer.optimization.final-effective-lrate $final_effective_lrate \
+        --egs.dir "$common_egs_dir" \
+        --cleanup.remove-egs $remove_egs \
+        --cleanup.preserve-model-interval 500 \
+        --use-gpu true \
+        --feat-dir=data/train_sp_hires \
+        --ali-dir $ali_dir \
+        --lang data/lang \
+        --dir=$exp_dir  || exit 1;
+
+#    has been deplicated
+#    steps/nnet3/train_tdnn.sh --num-epochs 4 --num-jobs-initial 2 --num-jobs-final 8 \
+#        --splice-indexes "-2,-1,0,1,2 -1,2 -3,3 -7,2 -3,3 0 0" \
+#        --feat-type raw \
+#        --online-ivector-dir exp/nnet3/ivectors_train_sp \
+#        --cmvn-opts "--norm-means=false --norm-vars=false" \
+#        --initial-effective-lrate 0.0015 --final-effective-lrate 0.00015 \
+#        --cmd "run.pl" \
+#        --relu-dim 850 \
+#        --stage -3 \
+#        data/train_sp_hires data/lang $ali_dir $exp_dir || exit 1;
 fi 
 
-if [ $stage -le 8 ]; then
+if [ $stage -le 6 ]; then
+    echo "$0: 6. ONLINE-DECODING..."
     steps/online/nnet3/prepare_online_decoding.sh --mfcc-config conf/mfcc_hires.conf 
         --add-pitch true data/lang exp/nnet3/extractor $exp_dir ${exp_dir}_online || exit 1;
 
--- train_ambase.sh
+++ train_ambase.sh
@@ -3,14 +3,6 @@
 
 cmd_train="run.pl"
 
-function score_report() {
-    [ $# -ne 1 ] && echo "function param mismatch..." && exit 1
-    if [[ $stage -le $1 && $score == true ]]; then
-        steps/scoring/score_kaldi_cer.sh --cmd $cmd_train $dev_dir $gph_dir $dec_dir || exit 1
-        # grep WER $dec_dir/cer_* | utils/best_wer.sh >> SCORE || exit 1
-        cat $dec_dir/scoring_kaldi/best_cer >> SCORE
-    fi 
-}
 
 function stop_or_continue() {
     [ $# -ne 1 ] && echo "function param mismatch..." && exit 1
@@ -18,25 +10,25 @@ function stop_or_continue() {
 }
 
 train=true
-score=true
 stage=1
 
 single_step=false
+decode_nj=10
 
-[ -z $KALDI_ROOT ] && echo "export KALDI_ROOT first" && exit 1
+[ -z $KALDI_ROOT ] && . ./path.sh
 
 source parse_options.sh || exit 1
 
-echo "train = $train; score = $score; single_step = $single_step; stage = $stage;"
+echo "train = $train; single_step = $single_step; stage = $stage;"
 
-#if [ $stage -le 1 ]; then
-#    echo "1. PREPARE FEATURE"
-#    for x in train dev; do
-#        steps/make_mfcc_pitch.sh --cmd $cmd_train --nj 10 data/$x exp/make_mfcc mfcc_pitch || exit 1    
-#        steps/compute_cmvn_stats.sh data/$x exp/make_mfcc mfcc_pitch || exit 1
-#    done
-#fi
-#
+if [ $stage -le 1 ]; then
+    echo "1. PREPARE FEATURE"
+    for x in train dev; do
+        steps/make_mfcc_pitch_online.sh --cmd $cmd_train --nj 10 data/$x exp/make_mfcc mfcc_pitch || exit 1    
+        steps/compute_cmvn_stats.sh data/$x exp/make_mfcc mfcc_pitch || exit 1
+    done
+fi
+
 #stop_or_continue 1
 # [ $stage -le 1 ] && [ $single_step == true ] && echo "step $stage done" && exit 
 
@@ -55,42 +47,43 @@ if [[ $stage -le 2 && $train == true ]]; then
     # utils/subset_data_dir.sh --first data/train 100000 data/tr100k || exit 1
     steps/train_mono.sh --cmd $cmd_train --nj 10 data/train data/lang $exp_dir || exit 1
     utils/mkgraph.sh data/lang_test $exp_dir $gph_dir || exit 1
-    steps/decode.sh --cmd $cmd_train --config conf/decode.config --nj 5 --skip_scoring true $gph_dir $dev_dir $dec_dir || exit 1
+    # using local/score.sh at the last of the script
+    steps/decode.sh --cmd $cmd_train --config conf/decode.config --nj $decode_nj $gph_dir $dev_dir $dec_dir || exit 1
 fi
 
-score_report 2 && stop_or_continue 2
+stop_or_continue 2
 
 exp_dir="exp/tri0"
 gph_dir="$exp_dir/graph"
 dec_dir="$exp_dir/decode"
 mdl_dir="exp/mono"
-ali_dir="exp/mono_align"
+ali_dir="exp/mono_ali"
 
 if [[ $stage -le 3 && $train == true ]]; then
     echo "3. TRIPLE_PHONE TRAINING 0"
     steps/align_si.sh --cmd $cmd_train --nj 10 data/train data/lang $mdl_dir $ali_dir || exit 1
     steps/train_deltas.sh --cmd $cmd_train 2500 20000 data/train data/lang $ali_dir $exp_dir || exit 1 
     utils/mkgraph.sh data/lang_test $exp_dir $gph_dir || exit 1
-    steps/decode.sh --cmd $cmd_train --config conf/decode.config --nj 5 --skip_scoring false $gph_dir $dev_dir $dec_dir || exit 1
+    steps/decode.sh --cmd $cmd_train --config conf/decode.config --nj $decode_nj $gph_dir $dev_dir $dec_dir || exit 1
 fi
 
-score_report 3 && stop_or_continue 3 
+stop_or_continue 3 
 
 exp_dir="exp/tri1"
 gph_dir="$exp_dir/graph"
 dec_dir="$exp_dir/decode"
 mdl_dir="exp/tri0"
-ali_dir="exp/tri0_align"
+ali_dir="exp/tri0_ali"
 
 if [[ $stage -le 4 && $train == true ]]; then
     echo "4. TRIPLE_PHONE TRAINING 1"
     steps/align_si.sh --cmd $cmd_train --nj 10 data/train data/lang $mdl_dir $ali_dir || exit 1
     steps/train_deltas.sh --cmd $cmd_train 2500 20000 data/train data/lang $ali_dir $exp_dir || exit 1 
     utils/mkgraph.sh data/lang_test $exp_dir $gph_dir || exit 1
-    steps/decode.sh --cmd $cmd_train --config conf/decode.config --nj 5 --skip_scoring true $gph_dir $dev_dir $dec_dir || exit 1
+    steps/decode.sh --cmd $cmd_train --config conf/decode.config --nj $decode_nj $gph_dir $dev_dir $dec_dir || exit 1
 fi
 
-score_report 4 && stop_or_continue 4 
+stop_or_continue 4 
 
 exp_dir="exp/lda_mllt"
 gph_dir="$exp_dir/graph"
@@ -98,7 +91,7 @@ dec_dir="$exp_dir/decode"
 # previous trained model
 mdl_dir="exp/tri1"
 # current output alignments direction
-ali_dir="exp/tri1_align"
+ali_dir="exp/tri1_ali"
 
 if [[ $stage -le 5 && $train == true ]]; then
     echo "5. LDA+MLLT TRAINING"
@@ -106,47 +99,48 @@ if [[ $stage -le 5 && $train == true ]]; then
     # steps/align_si.sh --cmd $cmd_train --nj 10 data/tr100k data/lang $mdl_dir ${ali_dir}_100k || exit 1
     steps/train_lda_mllt.sh --cmd $cmd_train 2500 20000 data/train data/lang $ali_dir $exp_dir || exit 1
     utils/mkgraph.sh data/lang_test $exp_dir $gph_dir || exit 1
-    steps/decode.sh --cmd $cmd_train --config conf/decode.config --nj 5 --skip_scoring true  $gph_dir $dev_dir $dec_dir || exit 1
+    steps/decode.sh --cmd $cmd_train --config conf/decode.config --nj $decode_nj $gph_dir $dev_dir $dec_dir || exit 1
 fi
 
-score_report 5 && stop_or_continue 5
+stop_or_continue 5
 
 # don't use align_si.sh for alignments and decode.sh for decoding
 exp_dir="exp/sat0"
 gph_dir="$exp_dir/graph"
 dec_dir="$exp_dir/decode"
 mdl_dir="exp/lda_mllt"
-ali_dir="exp/lda_mllt_align"
+ali_dir="exp/lda_mllt_ali"
 
 if [[ $stage -le 6 && $train == true ]]; then
     echo "6. SAT[SMALL] TRAINING"
     steps/align_fmllr.sh --cmd $cmd_train --nj 10 data/train data/lang $mdl_dir $ali_dir || exit 1
-    steps/train_sat.sh --cmd $cmd_train 2500 20000 data/train data/lang $ali_dir $exp_dir || exit 1
+    steps/train_sat.sh --cmd $cmd_train 3500 20000 data/train data/lang $ali_dir $exp_dir || exit 1
     utils/mkgraph.sh data/lang_test $exp_dir $gph_dir || exit 1
-    steps/decode_fmllr.sh --cmd $cmd_train --config conf/decode.config --nj 5 --skip_scoring true  $gph_dir $dev_dir $dec_dir || exit 1
+    steps/decode_fmllr.sh --cmd $cmd_train --config conf/decode.config --nj $decode_nj $gph_dir $dev_dir $dec_dir || exit 1
 fi
 
-score_report 6 && stop_or_continue 6
+stop_or_continue 6
 
 exp_dir="exp/sat1"
 gph_dir="$exp_dir/graph"
 dec_dir="$exp_dir/decode"
 mdl_dir="exp/sat0"
-ali_dir="exp/sat0_align"
+ali_dir="exp/sat0_ali"
 
 if [[ $stage -le 7 && $train == true ]]; then
     echo "7. SAT[BIG] TRAINING"
     steps/align_fmllr.sh --cmd $cmd_train --nj 10 data/train data/lang $mdl_dir $ali_dir || exit 1
-    steps/train_sat.sh --cmd $cmd_train 3500 100000 data/train data/lang $ali_dir $exp_dir || exit 1
+    steps/train_sat.sh --cmd $cmd_train 5000 100000 data/train data/lang $ali_dir $exp_dir || exit 1
     utils/mkgraph.sh data/lang_test $exp_dir $gph_dir || exit 1
-    steps/decode_fmllr.sh --cmd $cmd_train --config conf/decode.config --nj 10 --skip_scoring true $gph_dir $dev_dir $dec_dir || exit 1
+    steps/decode_fmllr.sh --cmd $cmd_train --config conf/decode.config --nj $decode_nj $gph_dir $dev_dir $dec_dir || exit 1
 fi
 
-score_report 7 && stop_or_continue 7
+stop_or_continue 7
 
 
 mdl_dir="exp/sat1"
-ali_dir="exp/sat1_align"
+ali_dir="exp/sat1_ali"
+echo "8. FINAL ALIGN USING SAT1"
 steps/align_fmllr.sh --cmd $cmd_train --nj 10 data/train data/lang $mdl_dir $ali_dir || exit 1
 
 echo "Done" && exit 0
@@ -158,17 +152,17 @@ ubm_dir="exp/ubm"
 gph_dir="$exp_dir/graph"
 dec_dir="$exp_dir/decode"
 mdl_dir="exp/sat1"
-ali_dir="exp/sgmm_align"
+ali_dir="exp/sgmm_ali"
 
 
 if [[ $stage -le 8 && $train == true ]]; then
-    echo "8. SGMM TRAINING"
+    echo "9. SGMM TRAINING"
     steps/align_fmllr.sh --cmd $cmd_train --nj 10 data/train data/lang $mdl_dir $ali_dir || exit 1
     steps/train_ubm.sh --cmd $cmd_train 900 data/train data/lang $ali_dir $ubm_dir || exit 1
     steps/train_sgmm2.sh --cmd $cmd_train 14000 35000 data/train data/lang $ali_dir $ubm_dir/final.ubm $exp_dir || exit 1
     utils/mkgraph.sh data/lang_test $exp_dir $gph_dir || exit 1
-    steps/decode_sgmm2.sh --cmd $cmd_train --nj 10 --config conf/decode.config --transform-dir exp/sat1/decode --skip_scoring true $gph_dir $dev_dir $dec_dir || exit 1;
+    steps/decode_sgmm2.sh --cmd $cmd_train --nj $decode_nj --config conf/decode.config --transform-dir exp/sat1/decode $gph_dir $dev_dir $dec_dir || exit 1;
 fi
 
-score_report 8 && stop_or_continue 8
+stop_or_continue 8
 
